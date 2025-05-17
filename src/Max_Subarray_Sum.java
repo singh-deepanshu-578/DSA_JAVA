5a3f322105c4a87bb07bc9035bdf0e4284cd06ab
SINGHDEEPANSHU578 <singhdeepanshu578@gmail.com>
2025-05-17 19:40:51 +0530

    17 may 2025

--- src/Max_Subarray_Sum.java
+++ src/Max_Subarray_Sum.java
@@ -2,7 +2,7 @@
 
 public class Max_Subarray_Sum {
 
-  public static void printMaxSubarraySum(int arr[]){
+  public static void printMaxSubarraySum_bf(int arr[]){
     int currSum=0;
     int maxSum=Integer.MIN_VALUE;
 
@@ -12,7 +12,6 @@ public class Max_Subarray_Sum {
         for(int k=i; k<=j; k++){
           currSum+=arr[k];
         }
-        System.out.println(currSum);
         if(maxSum<currSum){
           maxSum=currSum;
         }
@@ -20,8 +19,45 @@ public class Max_Subarray_Sum {
     }
     System.out.println("Max Sum = " + maxSum);
   }
+
+  //prefix sum method
+  public static void printMaxSubarray_prefix(int arr[]){
+    int currSum=0;
+    int maxSum=Integer.MIN_VALUE;
+    int prefix[]=new int[arr.length];
+    prefix[0]=arr[0];
+    for(int i=1;i<arr.length;i++){
+      prefix[i]=prefix[i-1]+arr[i];
+    }
+
+    for(int i=0;i<arr.length;i++){
+      for(int j=i;j<arr.length;j++){
+        currSum=i==0?prefix[j]:prefix[j]-prefix[i-1];
+        if(maxSum<currSum){
+          maxSum=currSum;
+        }
+      }
+    }
+    System.out.println("Max Sum = " + maxSum);
+  }
+
+  //kadane's algorithm
+
+  public static void kadane(int arr[]){
+    int maxSum=Integer.MIN_VALUE;
+    int currSum=0;
+
+    for(int i=0; i<arr.length; i++){
+      currSum = Math.max(arr[i], currSum+ arr[i]);
+      maxSum = Math.max(maxSum, currSum);
+    }
+
+    System.out.println("Max sum: " + maxSum);
+  }
   public static void main(String[] args) {
-    int arr[] = {1,-2,6,-1,3};
-    printMaxSubarraySum(arr);
+    int arr[] = {-1,-2,-3,-4};
+    printMaxSubarraySum_bf(arr);
+    printMaxSubarray_prefix(arr);
+    kadane(arr);
   }
 }
